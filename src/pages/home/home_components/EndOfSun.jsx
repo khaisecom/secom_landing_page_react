10b3cbf18c050f39ce37c076c1e6bab3b25c54ce
khaihv@secomvn.com <khaihv@secomvn.com>
2026-04-14 10:51:49 +0700

    fix : end of sun

--- src/pages/home/home_components/EndOfSun.jsx
+++ src/pages/home/home_components/EndOfSun.jsx
@@ -3,9 +3,17 @@ import sunImg from '../../../assets/images/top_red_sun.svg'
 function EndOfSun() {
   return (
     <div
-      className="w-full flex justify-center overflow-hidden bg-[#0c0c0c]"
+      className="relative w-full overflow-hidden bg-[#0c0c0c]"
       style={{ marginTop: '-2px' }}
     >
+      {/* Gradient overlay to blend top edge into background */}
+      <div
+        className="absolute top-0 left-0 right-0 z-10 pointer-events-none"
+        style={{
+          height: '30%',
+          background: 'linear-gradient(to bottom, #0c0c0c, transparent)',
+        }}
+      />
       <img
         src={sunImg}
         alt=""
@@ -16,6 +24,7 @@ function EndOfSun() {
           display: 'block',
           pointerEvents: 'none',
           userSelect: 'none',
+          opacity: 0.4
         }}
       />
     </div>
